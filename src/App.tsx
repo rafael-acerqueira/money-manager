b7875bd2544a81f91b76c992765ac960597b3d4e
Rafael Aquino <rafael.acerqueirah@gmail.com>
2021-09-14 19:23:16 -0300

    Create Header Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { Header } from './components/Header'
+
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <div className="App">
-      <h1>Hello baby</h1>
+    <>
+      <Header />
       <GlobalStyle />
-    </div>
+    </>
   )
 }
