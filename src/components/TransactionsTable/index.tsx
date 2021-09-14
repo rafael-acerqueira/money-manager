a887de1671db4f39c8c59a66841dac5d431fcc4c
Rafael Aquino <rafael.acerqueirah@gmail.com>
2021-09-14 20:04:07 -0300

    Add Transactions Table Component

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,36 @@
+import { Container } from "./styles";
+
+export const TransactionsTable = () => (
+  <Container>
+    <table>
+      <thead>
+        <tr>
+          <th>Título</th>
+          <th>Valor</th>
+          <th>Categoria</th>
+          <th>Data</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Lavagem Simples</td>
+          <td className='withdraw'>-R$100,00</td>
+          <td>Higiene</td>
+          <td>20/03/2020</td>
+        </tr>
+        <tr>
+          <td>Lavagem Completa</td>
+          <td className='withdraw'>-R$250,00</td>
+          <td>Higiene</td>
+          <td>20/01/2021</td>
+        </tr>
+        <tr>
+          <td>Salário</td>
+          <td className='deposit'>R$1.500,00</td>
+          <td>Financeiro</td>
+          <td>23/06/2021</td>
+        </tr>
+      </tbody>
+    </table>
+  </Container>
+)
