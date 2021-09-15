59d2ddc913dbb587947ad3c3c94776d485effdde
Rafael Aquino <rafael.acerqueirah@gmail.com>
2021-09-15 17:37:46 -0300

    Install and configure MirageJS to fake an API

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,36 +1,46 @@
+import { useEffect } from "react";
 import { Container } from "./styles";
 
-export const TransactionsTable = () => (
-  <Container>
-    <table>
-      <thead>
-        <tr>
-          <th>Título</th>
-          <th>Valor</th>
-          <th>Categoria</th>
-          <th>Data</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Lavagem Simples</td>
-          <td className='withdraw'>-R$100,00</td>
-          <td>Higiene</td>
-          <td>20/03/2020</td>
-        </tr>
-        <tr>
-          <td>Lavagem Completa</td>
-          <td className='withdraw'>-R$250,00</td>
-          <td>Higiene</td>
-          <td>20/01/2021</td>
-        </tr>
-        <tr>
-          <td>Salário</td>
-          <td className='deposit'>R$1.500,00</td>
-          <td>Financeiro</td>
-          <td>23/06/2021</td>
-        </tr>
-      </tbody>
-    </table>
-  </Container>
-)
+export const TransactionsTable = () => {
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/transactions')
+      .then(reponse => reponse.json())
+      .then(data => console.log(data))
+  }, [])
+
+  return(
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Lavagem Simples</td>
+            <td className='withdraw'>-R$100,00</td>
+            <td>Higiene</td>
+            <td>20/03/2020</td>
+          </tr>
+          <tr>
+            <td>Lavagem Completa</td>
+            <td className='withdraw'>-R$250,00</td>
+            <td>Higiene</td>
+            <td>20/01/2021</td>
+          </tr>
+          <tr>
+            <td>Salário</td>
+            <td className='deposit'>R$1.500,00</td>
+            <td>Financeiro</td>
+            <td>23/06/2021</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer } from 'miragejs'
 import { App } from './App';
+
+createServer({
+  routes(){
+    this.namespace = 'api'
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 200,
+          type: 'deposit',
+          category: 'Food',
+          date: '2021-09-13'
+        }
+      ]
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
